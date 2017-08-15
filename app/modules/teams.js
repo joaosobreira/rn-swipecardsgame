c17b3880131fdca07b182c6db09da3b03802300d
joaosobreira <joaosobreira@gmail.com>
2017-08-15 02:37:06 +0100

    Added functionality of more than 2 teams

--- app/modules/teams.js
+++ app/modules/teams.js
@@ -11,12 +11,31 @@ export const EDIT_PLAYER_NAME = 'EDIT_PLAYER_NAME'
 export const GO_TO_NEXT_PLAYER = 'GO_TO_NEXT_PLAYER'
 export const GO_TO_NEXT_TEAM = 'GO_TO_NEXT_TEAM'
 export const ADD_POINT_TO_ACTIVE_TEAM = 'ADD_POINT_TO_ACTIVE_TEAM'
+export const SET_NUMBER_OF_TEAMS = 'SET_NUMBER_OF_TEAMS'
+
+const defaultTeamNames = ["Team C", "Team D", "Team E", "Team F"];
+
 
 // action Creators
 export function addTeam(teamName){
-  return {type: ADD_TEAM,
-  payload: teamName
-		}
+  return {
+    type: ADD_TEAM,
+    payload: teamName
+	}
+}
+
+export function setNumberOfTeams(numberOfTeams){
+  let newTeams = []
+  if(numberOfTeams>2){
+    for(i = 0; i<(numberOfTeams-2); i++){
+      newTeams.push({name: defaultTeamNames[i], points: 0})
+    }
+  }
+  console.log('newTeams: ',newTeams)
+  return {
+    type: SET_NUMBER_OF_TEAMS,
+    payload: newTeams
+	}
 }
 
 export function addPlayerToTeam(teamName,newPlayerName){
@@ -65,6 +84,7 @@ export function addPointToActiveTeam(){
 // selectors
 export const getPointsByTeam = (state, team) => state.teams[team].points
 export const getActiveTeamName = (state) => state.teams[state.activeTeam].name
+export const getNumberOfTeams = (state) => state.teams.length
 
 
 // reducers
@@ -96,9 +116,15 @@ export default function reducer (state = initialState, action) {
           )
         }
       case GO_TO_NEXT_TEAM:
+        console.log(getNumberOfTeams(state))
         return {
           ...state,
-          activeTeam: state.activeTeam==0 ? 1 : 0
+          activeTeam: (state.activeTeam+1)%getNumberOfTeams(state)
+        }
+      case SET_NUMBER_OF_TEAMS:
+        return {
+          ...state,
+          teams: state.teams.concat(action.payload)
         }
       default:
         return state;
--- app/screens/Configs.js
+++ app/screens/Configs.js
@@ -2,24 +2,68 @@ import React, {Component} from 'react';
 import {
   View,
   StyleSheet,
-  Text
+  Text,
+  Dimensions
 } from 'react-native';
-import {Button} from 'native-base';
+import { Container, Content, Picker, Button } from 'native-base';
+import {connect} from 'react-redux'
 
-export default class Home extends Component{
+import {setNumberOfTeams} from '../modules/teams'
+
+class Configs extends Component{
 
   static navigationOptions = {
     header: null
   }
 
+  constructor(props){
+    super(props);
+    this.state = {
+      selectedItem: 2
+    }
+  }
+
+  _onValueChange(value){
+    this.setState({selectedItem: value})
+  }
+
+  _onSaveButton(){
+    console.log('running save button: ',this.props)
+    this.props.dispatch(setNumberOfTeams(this.state.selectedItem))
+    this.props.navigation.navigate('Home')
+  }
+
   render(){
 
+    let btnWidth = Dimensions.get('window').width/3
     const { navigate } = this.props.navigation;
 
     return(
       <View style={styles.container}>
+        <Text>Number of Teams</Text>
+        <Container>
+          <Content>
+              <Picker
+                  style={{flex: 1, width: 100}}
+                  iosHeader="Select one"
+                  mode="dropdown"
+                  selectedValue={this.state.selectedItem}
+                  onValueChange={this._onValueChange.bind(this)}>
+                  <Picker.Item label="2" value={2} />
+                  <Picker.Item label="3" value={3} />
+                  <Picker.Item label="4" value={4} />
+                  <Picker.Item label="5" value={5} />
+             </Picker>
+          </Content>
+        </Container>
+
+        <View style={[styles.btnContainer, {width: btnWidth}]}>
+          <Button block style={{margin: 10}} onPress={this._onSaveButton.bind(this)}><Text>Save</Text></Button>
+        </View>
 
       </View>
+
+
 
     )
 
@@ -42,3 +86,12 @@ const styles = StyleSheet.create({
     alignItems: 'center'
   }
 })
+
+const mapStateToProps = state => {
+  //console.log('state round: ',state)
+  return {
+    teams: state.teams,
+  }
+}
+
+export default connect(mapStateToProps)(Configs)
